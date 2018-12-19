b970ecad2fad56091975eea1c1ee801d1de861c2
colmer <colmer@yandex.ru>
2018-12-19 05:11:58 +0400

    Update axios util

--- utils/axios.js
+++ utils/axios.js
@@ -1,79 +1,105 @@
 import axios from 'axios';
 import cookie from 'js-cookie';
 
-// const token = cookie.get('token');
-// if (token) axios.defaults.headers.common['Authorization'] = `Berear ${token}`;
 axios.defaults.baseURL = process.env.API;
 
+// class Axios {
+//   constructor(options = {}) {
+//     this.client = options.client || axios.create();
+//     this.token = options.token;
+//     this.refreshToken = options.refreshToken;
+//     this.refreshRequest = null;
+
+//     this.client.interceptors.request.use(
+//       config => {
+//         if (!this.token) {
+//           return config;
+//         }
+
+//         const newConfig = {
+//           headers: {},
+//           ...config,
+//         };
+
+//         newConfig.headers.Authorization = `Bearer ${this.token}`;
+//         return newConfig;
+//       },
+//       e => Promise.reject(e),
+//     );
+
+//     this.client.interceptors.response.use(
+//       r => r,
+//       async error => {
+//         if (!this.refreshToken || error.response.status !== 401 || error.config.retry) {
+//           throw error;
+//         }
+
+//         if (!this.refreshRequest) {
+//           this.refreshRequest = this.client.post('/auth/refresh', {
+//             refreshToken: this.refreshToken,
+//           });
+//         }
+//         const { data } = await this.refreshRequest;
+//         this.token = data.token;
+//         this.refreshToken = data.refreshToken;
+//         const newRequest = {
+//           ...error.config,
+//           retry: true,
+//         };
+
+//         return this.client(newRequest);
+//       },
+//     );
+//   }
+// }
+
+// const updatedAxios = new Axios({
+//   token: cookie.get('token'),
+//   refreshToken: cookie.get('refreshToken'),
+// });
+
+// export default updatedAxios.client;
+
 axios.interceptors.request.use(
-  function(config) {
-    // config.headers.common['Authorization'] = `Bearer ${token}`;
+  config => {
     const token = cookie.get('token');
-    if (token) config.headers['Authorization'] = `Bearer ${token}`;
-    console.log('config', config);
-    return config;
+
+    if (!token) return config;
+
+    const newConfig = {
+      headers: {},
+      ...config,
+    };
+
+    newConfig.headers.Authorization = `Bearer ${token}`;
+    return newConfig;
   },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  },
+  error => Promise.reject(error),
 );
 
 axios.interceptors.response.use(
-  function(response) {
-    console.log('Response', response);
-    // Do something with response data
-    return response;
-  },
-  async function(err) {
-    const res = err.response;
-    const token = cookie.get('token');
-    if ((res.status === 401 || res.status === 403) && token) {
-      console.log('Remove token');
-      cookie.remove('token');
-
-      await axios.put('/token', { refreshToken: cookie.get('refreshToken') });
-      console.log('Request');
+  r => r,
+  async err => {
+    const res = err.response || {};
+    const refreshToken = cookie.get('refreshToken');
+    if (!refreshToken || res.status !== 401 || err.config.retry) {
+      throw err;
     }
-    console.log('Error', err);
-    // Do something with response error
-    return Promise.reject(err);
+
+    const { data } = await axios.post('/auth/refresh', {
+      refreshToken: cookie.get('refreshToken'),
+    });
+
+    cookie.set('token', data.token);
+    cookie.set('refreshToken', data.refreshToken);
+
+    const newRequest = {
+      ...err.config,
+      retry: true,
+    };
+
+    return axios.request(newRequest);
   },
 );
-
-// const client = axios.create({
-//   baseURL: process.env.API,
-// });
-
-// const request = function(options) {
-//   // console.log('Request');
-//   // return await client(options);
-//   //     const onSuccess = function(response) {
-//   //     console.debug('Request Successful!', response);
-//   //     return response.data;
-//   //   };
-
-//   //   const onError = function(error) {
-//   //     console.error('Request Failed:', error.config);
-
-//   //     if (error.response) {
-//   //       // Request was made but server responded with something
-//   //       // other than 2xx
-//   //       console.error('Status:', error.response.status);
-//   //       console.error('Data:', error.response.data);
-//   //       console.error('Headers:', error.response.headers);
-//   //     } else {
-//   //       // Something else happened while setting up the request
-//   //       // triggered the error
-//   //       console.error('Error Message:', error.message);
-//   //     }
-
-//   //     return Promise.reject(error.response || error.message);
-//   //   };
-
-//   return client(options)
-//     .then(onSuccess)
-//     .catch(onError);
-// };
 
 export default axios;
